23cafba52aa288377e49d919f6e336a28f3bc99f
RichardJCase <johndoe27328@gmail.com>
2016-04-28 13:04:48 -0500

    removing old files

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:
-	g++ main.cc -lGL -lGLU -lglut -lsfml-audio -lpthread -O3 -Wno-write-strings -Wno-format
-	rm *~
--- src/error.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define ERRSTAMP __DATE__, __TIME__, __FILE__, __LINE__
-
-//meant for errors, but will be used for general purposes as well
-void msg(char *imsg, char *date, char *time,
-	 char *file, unsigned int line);
